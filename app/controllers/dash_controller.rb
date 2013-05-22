7c6c8cd501c699cf79857997061b251959ef1294
TSMMark <marka@trendsettermarketing.net>
2013-05-22 16:49:09 -0400

    Add comments to Ownable

--- app/controllers/dash_controller.rb
+++ app/controllers/dash_controller.rb
@@ -1,5 +1,7 @@
 class DashController < ApplicationController
+  
   def index
-  	
+    @pages = current_user.pages
   end
+
 end
--- lib/ownable.rb
+++ lib/ownable.rb
@@ -1,45 +1,48 @@
 module Ownable
 
+  # permission methods
   def ownable?
     true
   end
 
+  # get array of owners
   def get_owners
     self.users
   end
+  # get last owner
+  def get_owner
+    self.get_owners.last
+  end
 
+  # alias get_owner
+  def owner
+    self.get_owner
+  end
+  # alias get_owners
   def owners
     self.get_owners
   end
 
-  def get_owner
-    self.get_owners.last
-  end
-  def owner
-    self.get_owner
-  end
-
+  # return true if a user is an owner of the Ownable
   def user_is_owner? user=current_user
     self.get_owners.include? user
   end
 
-  # returns true if a user is an OWNER or an ADMIN
+  # returns true if a user is an OWNER or an ADMIN of the Ownable
   def user_is_admin_or_owner? user=current_user
     user.admin? || self.user_is_owner?(user)
   end
 
+  # CRUD permissions
   def user_can_create? user=current_user
     user_is_admin_or_owner? user
   end
-
   def user_can_read? user=current_user
     user_is_admin_or_owner? user
   end
-
   def user_can_update? user=current_user
     user_is_admin_or_owner? user
   end
-
   def user_can_destroy? user=current_user
     user_is_admin_or_owner? user
   end
